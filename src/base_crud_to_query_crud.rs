dc8107526aac4a3e5c049360b17350d7de72a28e
nb-midwestern <103958286+nb-midwestern@users.noreply.github.com>
2024-06-25 15:11:39 -0500

    feat: add field and type

--- src/base_crud_to_query_crud.rs
+++ src/base_crud_to_query_crud.rs
@@ -4,7 +4,7 @@ use crate::utils::{
     get_namespace::get_base_namespace,
 };
 
-pub fn run(content: String, entity_id_name: Option<String>) -> String {
+pub fn run(content: String, entity_id_name: Option<String>, sortable_fields: Option<Vec<String>>) -> String {
     let entity_id_name = entity_id_name.unwrap_or("<REPLACE_WITH_ENTITY_ID_NAME>".to_string());
 
     let base_project_route =
@@ -19,13 +19,15 @@ pub fn run(content: String, entity_id_name: Option<String>) -> String {
         entity_name.clone(),
         entity_id_name.clone(),
         base_namespace.clone(),
+        sortable_fields.clone(),
     );
     let query_criteria_class = generate_query_criteria(
         entity_name.clone(),
         entity_id_name.clone(),
         base_namespace.clone(),
+        sortable_fields.clone(),
     );
-    let new_repo = new_repository_name(content.clone());
+    let new_repo = new_repository_name(content.clone(),         sortable_fields.clone(),);
     let new_interface = new_repo_interface_name(entity_name.clone());
     return format!(
         r#"
@@ -66,9 +68,15 @@ fn generate_sortable_field_enum(
     entity_name: String,
     entity_id_name: String,
     namespace: Option<String>,
+    sortable_fields: Option<Vec<String>>,
 ) -> String {
     let namespace = match namespace {
         Some(base_namespace) => format!("namespace {base_namespace}.Core.Domain;"),
+        None => "".to_string(),
+    };
+
+    let sortable_fields:String = match sortable_fields {
+        Some(fields) => fields.clone().into_iter().map(|field| {return format!("{field}, \n")}).collect(),
         None => "".to_string(),
     };
     return format!(
@@ -80,6 +88,7 @@ public enum {entity_name}SortableField
     {entity_id_name},
     EffectiveDate,
     ExpireDate, 
+    {sortable_fields}
 }}
 "#,
     );
@@ -98,7 +107,14 @@ fn generate_query_criteria(
     entity_name: String,
     entity_id_name: String,
     namespace: Option<String>,
+    sortable_fields: Option<Vec<String>>,
 ) -> String {
+
+    let sortable_fields:String = match sortable_fields {
+        Some(fields) => fields.clone().into_iter().map(|field| {return format!("public TYPE? {field} {{ get; set; }} \n")}).collect(),
+        None => "".to_string(),
+    };
+
     let namespace = match namespace {
         Some(base_namespace) => format!("namespace {base_namespace}.Core.Domain;"),
         None => "".to_string(),
@@ -110,6 +126,7 @@ fn generate_query_criteria(
 
 public class  {entity_name}QueryCriteria : DefaultQueryCriteria<{entity_name}SortableField>
 {{
+    {sortable_fields}
     public override SortCriteria<{entity_name}SortableField> SortCriteria {{ get; set; }} = new()
     {{
         {{ {entity_name}SortableField.{entity_id_name}, SortOrder.Ascending }}
@@ -119,10 +136,15 @@ public class  {entity_name}QueryCriteria : DefaultQueryCriteria<{entity_name}Sor
     );
 }
 
-fn new_repository_name(file_content: String) -> String {
+fn new_repository_name(file_content: String, sortable_fields: Option<Vec<String>>) -> String {
+    let has_sortable_fields = match sortable_fields {
+        Some(_) => {true},
+        None => {false},
+    };
     let entity_name = extract_entity_from_base_crud_repo_class(file_content.clone()).unwrap();
     let (class_name, class_line_number) =
         get_class_name_and_line_number(file_content.clone()).unwrap();
+
 
     let res: String = file_content
         .lines()
@@ -130,6 +152,20 @@ fn new_repository_name(file_content: String) -> String {
         .map(|(index, line)| {
             if index == class_line_number  {
                 return format!("\n internal class {class_name} : BaseQueryCrudRepository<{entity_name}, {entity_name}QueryCriteria, {entity_name}SortableField>, I{class_name}");
+            } else if has_sortable_fields && line.trim() == "}" && file_content.lines().skip(index + 1).all(|l| l.trim().is_empty()) {
+                let query:String = sortable_fields.clone().unwrap().into_iter().map(|field| {
+                    return format!(".WhereIf(criteria.{field}.HasValue, e => e.{field} == criteria.{field})")
+                }).collect();
+                return format!(
+r#"
+    protected override IQueryable<{entity_name}> ApplyCriteria(IQueryable<{entity_name}> query, {entity_name}QueryCriteria criteria)
+    {{
+        //TODO check these for correctness. Strings should be string.IsNullOrEmpty(field), arrays should be field.Any
+        return query{query};
+    }}
+}}
+"#
+)
             } 
             
             format!("\n{line}")
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use clap::Parser;
 use dotenv::dotenv;
+use itertools::Itertools;
 use serde::Serialize;
 use std::fs::File;
 use std::io::{self, Read, Write};
 use std::path::Path;
+use std::str::FromStr;
 mod base_crud_to_query_crud;
 mod c_sharp_dto_to_ts_interface;
 
@@ -14,6 +16,30 @@ mod utils;
 enum Programs {
     CsDtoToTsInterface,
     GenerateQueryCriteriaFromBaseCrudClass,
+}
+
+#[derive(Clone, Debug)]
+struct FieldWithType {
+    field: String,
+    field_type: String,
+}
+
+impl FromStr for FieldWithType {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (field, field_type) = s
+            .split(",")
+            .collect::<Vec<_>>()
+            .into_iter()
+            .collect_tuple()
+            .unwrap();
+
+        Ok(Self {
+            field: field.to_string(),
+            field_type: field_type.to_string(),
+        })
+    }
 }
 
 #[derive(Parser, Debug)]
@@ -36,11 +62,18 @@ struct Args {
 
     #[arg(short, long)]
     base_name_space: Option<String>,
+
+    #[clap(short, long, value_parser, num_args = 1.., value_delimiter = ' ')]
+    sortable_fields: Option<Vec<String>>,
+
+    #[clap(short, long, value_parser, num_args = 1.., value_delimiter = ' ')]
+    type_sortable_fields: Option<Vec<FieldWithType>>,
 }
 
 fn main() -> io::Result<()> {
     dotenv().ok();
     let args = Args::parse();
+    println!("{:?}", args.type_sortable_fields);
 
     // Open the input file and read its contents
     let mut input_file = File::open(&Path::new(args.in_file.as_str()))?;
@@ -53,7 +86,7 @@ fn main() -> io::Result<()> {
             c_sharp_dto_to_ts_interface::csharp_dto_to_ts_interface(input_content)
         }
         Programs::GenerateQueryCriteriaFromBaseCrudClass => {
-            base_crud_to_query_crud::run(input_content, args.entity_id_name)
+            base_crud_to_query_crud::run(input_content, args.entity_id_name, args.sortable_fields)
         }
     };
 
